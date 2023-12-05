52995a901cd5218df7e5a4ef3cf7a53e03b11a26
Ben_Argaman <94112749+benalon98@users.noreply.github.com>
2023-12-05 14:05:12 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,25 +1,28 @@
-import "./globals.css";
+import Head from 'next/head';
+import './globals.css';
 
 export const metadata = {
   title: "א.א הרמות | שירותי מנוף והרמת משאות",
-
-  description:
-    "שירותי מנוף והרמת משאות עד 15 קומות, מעברי דירה או משרד, חסכוני בכסף, חסכוני בזמן, זמינות גבוהה בפריסה ארצית",
+  description: "שירותי מנוף והרמת משאות עד 15 קומות, מעברי דירה או משרד, חסכוני בכסף, חסכוני בזמן, זמינות גבוהה בפריסה ארצית",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="he" dir="RTL">
+    <>
+      <Head>
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-8NK0GE4NKD"></script>
-      <script>
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-8NK0GE4NKD');
-        `}
-      </script>
-      <body>{children}</body>
-    </html>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-8NK0GE4NKD');
+          `}
+        </script>
+      </Head>
+      <html lang="he" dir="RTL">
+        <body>{children}</body>
+      </html>
+    </>
   );
 }
